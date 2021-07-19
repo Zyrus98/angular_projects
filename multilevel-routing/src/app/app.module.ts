9254f9117db3931c020fcadbaabb282bd7aee106
Shailendra Kumar Soni <shailendrasoni998@gmail.com>
2021-07-19 09:54:36 +0530

    latest_update

--- multilevel-routing/src/app/app.module.ts
+++ multilevel-routing/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { Route1Component } from './route1/route1.component';
+import { Route2Component } from './route2/route2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    Route1Component,
+    Route2Component
   ],
   imports: [
     BrowserModule,
